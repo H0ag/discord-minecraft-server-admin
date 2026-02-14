00d18f49ab1789fcf471ae41524c1bd9f16f81ca
h0ag <technologie.ma3l@gmail.com>
2026-02-14 18:05:15 +0100

    Translated code in english

--- bot.py
+++ bot.py
@@ -15,16 +15,16 @@ class Saladia(discord.Client):
 
     async def setup_hook(self):
         await self.tree.sync()
-        print(f"Commandes synchronisées !")
+        print(f"Synced commands !")
 
     async def on_ready(self):
-        print(f'Connecté en tant que {self.user} (ID: {self.user.id})')
+        print(f'Logged as {self.user} (ID: {self.user.id})')
 
 bot = Saladia()
 
 # Command "/server-update"
-@bot.tree.command(name="server-update", description="Demarrer la mise à jour du serveur")
-@app_commands.describe(version="La version de destination (ex: 1.21.1)")
+@bot.tree.command(name="server-update", description="Start server update")
+@app_commands.describe(version="Wanted version (e.g.: 1.21.1)")
 async def server_update(interaction: discord.Interaction, version: str):
     # On répond à l'utilisateur
     await interaction.response.send_message(f"Server will update to version {version}")
@@ -32,4 +32,4 @@ async def server_update(interaction: discord.Interaction, version: str):
 if TOKEN:
     bot.run(TOKEN)
 else:
-    print("Erreur : DISCORD_TOKEN non trouvé dans le fichier .env")
+    print("Erreur : DISCORD_TOKEN not found in .env")
